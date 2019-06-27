8b4fed04a128c7cc501bbb966f93dce9f86ee9e1
Andrie Tri Laksono <andrietrilaksono@gmail.com>
2019-06-27 19:57:54 +0700

    Create seeder category
    Create seeder category

---
+++ database/seeds/CategoryTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Entities\Category;
+use Carbon\Carbon;
+
+class CategoryTableSeeder extends Seeder
+{
+    private const CATEGORY_ARTICLE_TABLE = 'categories';
+
+    public function run()
+    {
+        $mytime = Carbon::now();
+        DB::table(self::CATEGORY_ARTICLE_TABLE)->delete();
+        $category = [
+            [
+                'id' => 1,
+                'name' => 'Fashion',
+                'slug' => 'fashion',
+                'isActive' => 1,
+                'createdDate' => $mytime,
+                'createdBy' => 1,
+                'updatedDate' => null,
+                'updatedBy' => null,
+                'isDeleted' => 0,
+                'deletedDate' => null
+            ],
+            [
+                'id' => 2,
+                'name' => 'Viral',
+                'slug' => 'viral',
+                'isActive' => 1,
+                'createdDate' => $mytime,
+                'createdBy' => 1,
+                'updatedDate' => null,
+                'updatedBy' => null,
+                'isDeleted' => 0,
+                'deletedDate' => null
+            ]
+        ];
+
+        foreach($category as $data){
+            Category::create($data);
+        }
+    }
+}
