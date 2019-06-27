b475e59a1df056ddde596ab6a25530b4277d7db9
Andrie Tri Laksono <andrietrilaksono@gmail.com>
2019-06-27 20:00:54 +0700

    Edit controller
    Edit controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Response;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    /**
+     * @var Illuminate\Http\Request
+     */
+    protected $request;
+
+    /**
+     * Status code
+     *
+     * @var int
+     */
+    protected $statusCode = 0;
+
+    /**
+     * Status message
+     *
+     * @var string
+     */
+    protected $statusMessage = 'Success';
+
+    /**
+     * Http status code
+     *
+     * @var int
+     */
+    protected $httpStatusCode = Response::HTTP_OK;
 }
